48fdeaedea554ff19aa174efc0ddcfed54f03804
Warren Hartzler <whrzl5@aol.com>
2025-02-02 16:23:00 -0700

    Update Feth_YOY_Review.sql

--- Code Files/Feth_YOY_Review.sql
+++ Code Files/Feth_YOY_Review.sql
@@ -1,9 +1,12 @@
 SELECT 
     EXTRACT(YEAR FROM created_date) AS year, 
     COUNT(DISTINCT id) AS user_count,
-    ROUND(
-        (COUNT(DISTINCT id) - LAG(COUNT(DISTINCT id)) OVER (ORDER BY EXTRACT(YEAR FROM created_date))) * 100.0 / 
-        NULLIF(LAG(COUNT(DISTINCT id)) OVER (ORDER BY EXTRACT(YEAR FROM created_date)), 0), 2
+    TO_CHAR(
+        ROUND(
+            (COUNT(DISTINCT id) - LAG(COUNT(DISTINCT id)) OVER (ORDER BY EXTRACT(YEAR FROM created_date))) * 100.0 / 
+            NULLIF(LAG(COUNT(DISTINCT id)) OVER (ORDER BY EXTRACT(YEAR FROM created_date)), 0), 
+        2), 
+        'FM999.00"%"'
     ) AS yoy_growth_percentage
 FROM public.user
 GROUP BY year
