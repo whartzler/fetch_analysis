7b35619387db7cef2657c3848122dcbba04e2521
Warren Hartzler <whrzl5@aol.com>
2025-02-02 15:53:20 -0700

    Update Top_Dip&Salsa_Brand.sql

--- Code Files/Top_Dip&Salsa_Brand.sql
+++ Code Files/Top_Dip&Salsa_Brand.sql
@@ -1,6 +1,6 @@
-select brand,sum(t.final_sale) FROM PUBLIC.transactions as t
+select brand,sum(t.final_sale) as sum_final_sales FROM PUBLIC.transactions as t
 join public.products as p on t.barcode = p.barcode
-where p.category_2 = ''
+where p.category_2 = 'Dips & Salsa'
 and brand is not null
 group by 1
 order by sum(t.final_sale) desc
